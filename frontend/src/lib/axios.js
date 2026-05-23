1c212f8085a627a21c2e47688d3fafaed45e0347
lets-make-ashwani <ashvish420waniwakarma.com>
2026-05-23 22:41:44 +0530

    Update API URL to live Render backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    // baseURL: 'http://127.0.0.1:8000/api/',
+      baseURL: 'https://edudesk-api.onrender.com/',
 });
 
 api.interceptors.request.use((config) => {
