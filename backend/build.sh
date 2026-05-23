b853ba285e965e9475688ea2363bc5ebb0e6e804
lets-make-ashwani <ashvish420waniwakarma.com>
2026-05-24 00:32:38 +0530

    Add makemigrations to build script to fix 500 errors

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations --noinput
 python manage.py migrate
 
 # Automatically create superuser (fails gracefully if it already exists)
