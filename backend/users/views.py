332d4597292a1205a9abaf76eee648ab0cb0314b
lets-make-ashwani <ashvish420waniwakarma.com>
2026-05-26 17:27:25 +0530

    fix: isolate UserViewSet to current school and hide platform SUPERADMIN from school admins

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,9 +6,25 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.none()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.role == 'SUPERADMIN':
+            return User.objects.all()
+        if user.school:
+            # Exclude superadmins and return only users within the same school
+            return User.objects.filter(school=user.school).exclude(role='SUPERADMIN')
+        return User.objects.none()
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        if user.role != 'SUPERADMIN' and user.school:
+            serializer.save(school=user.school)
+        else:
+            serializer.save()
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
